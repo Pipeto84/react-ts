ed7a981d10cfe3db20fae1a6f1144bf821549e3d
Pipeto84 <pipejarauribeboteroa@gmail.com>
2025-12-31 00:08:23 -0500

    tasks column

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,6 +1,11 @@
+import Button from '@mui/material/Button'
+import "../styles/TasksPage.css"
 
 export const TasksPage = () => {
   return (
-    <div>TasksPage</div>
+    <div className='tasks'>
+      <h1>Tasks</h1>
+      <Button href="/" variant="contained" size='large'>Create new list</Button>
+    </div>
   )
 }
