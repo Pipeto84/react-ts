cdb654c1a9eb2bf412debc8ffc28e81aff77fb01
Pipeto84 <pipejarauribeboteroa@gmail.com>
2025-12-30 22:08:04 -0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { NavBar } from "./components/NavBar";
+import { Navigate, Route, Routes, BrowserRouter} from "react-router-dom";
+import { TasksPage } from './pages/TasksPage';
+import './styles/App.css';
+
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="container-main flex">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 
-export default App
+}
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import "../styles/NavBar.css";
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <div className="topnav">
+      <NavLink className="active" to="/">
+        Home
+      </NavLink>
+      <NavLink className="active" to="/tasks">
+        Tasks
+      </NavLink>
+    </div>
+  );
+};
