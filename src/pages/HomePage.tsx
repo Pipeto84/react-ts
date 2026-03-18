9e2a65d332894ae212d1cbd0c460ddf16a782cce
Pipeto84 <pipejarauribeboteroa@gmail.com>
2026-03-18 10:51:02 -0500

    introduccion home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,8 +8,14 @@ export const HomePage = () => {
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
       <a href="https://github.com/Pipeto84" target="_blank" className="aTitulo">
-        <h1 className="h1Home">React TS Pipeto84</h1>
+        <h1 className="h1Home">React</h1>
       </a>
+      <p className="infoHome">
+        This website is designed in React with TypeScript. Here you'll find a
+        store with a shopping cart, employee scheduling with drag and drop, user
+        login and registration, search bars, and a task lists.<br /> Designed by Felipe
+        Jaramillo U. ("Pipeto84" GitHub)
+      </p>
     </div>
   );
 };
