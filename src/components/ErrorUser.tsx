e5bce0bcd9292de5efc0ccffb7c2aa19ef0431df
Felipe Jaramillo <pipejarauribeboteroa@gmail.com>
2026-04-07 18:16:48 -0500

    error user  i18n listo

--- src/components/ErrorUser.tsx
+++ src/components/ErrorUser.tsx
@@ -1,19 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import {useTranslation} from 'react-i18next'
 import "../styles/User.css";
 
 export function ErrorUser() {
   const navigate = useNavigate();
+  const { t } = useTranslation("global");
   const handleRegresar = () => {
     navigate("/user");
   };
   return (
     <div className="error">
       <div className="contenido-error">
-        <h3 className="titulo-error">Error</h3>
-        <p className="texto-error">The name or password</p>
-        <p className="texto-error">are incorrect</p>
+        <h3 className="titulo-error">{t("signIn.errorTitle")}</h3>
+        <p className="texto-error">{t("signIn.errorMessage")}</p>
         <button className="boton-error" onClick={handleRegresar}>
-          Return
+          {t("signIn.btnTryAgain")}
         </button>
       </div>
     </div>
