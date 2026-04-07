f3d3c19b8dbeded8078f151775e73d4e804db1ce
Felipe Jaramillo <pipejarauribeboteroa@gmail.com>
2026-04-07 18:10:25 -0500

    register i18n listo

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import "../styles/Register.css";
 import { useAppDispatch } from "../app/hooks";
 import { NavLink } from "react-router-dom";
 import { addUser } from "../features/user/usersSlice";
 import { setLogIn } from "../features/user/logInSlice";
+import { useTranslation } from "react-i18next";
+import "../styles/Register.css";
 
 export function Register() {
+  const { t } = useTranslation("global");
   const dispatch = useAppDispatch();
   const [added, setAdded] = useState(false);
   const [newUser, setNewUser] = useState({
@@ -29,74 +31,78 @@ export function Register() {
       setNewUser({ userName: "", password: "" });
     }
   };
-  const handleClass=() => {
-    if(added){
+  const handleClass = () => {
+    if (added) {
       return "registrado";
-    }else{
+    } else {
       return "registro";
     }
-  }
+  };
   return (
     <div className={handleClass()}>
       {!added && (
         <div className="contenido-registro">
-          <h3 className="titulo-registro">New User</h3>
+          <h3 className="titulo-registro">{t("register.title")}</h3>
           <div className="formulario-registro">
-            <label className="label-registro">Name</label>
+            <label className="label-registro">{t("register.nameLabel")}</label>
             <input
               type="text"
-              placeholder="Your name"
+              placeholder={t("register.nameInput")}
               className="input-registro"
               autoFocus
             />
-            <label className="label-registro">Last Name</label>
+            <label className="label-registro">
+              {t("register.lastNameLabel")}
+            </label>
             <input
               type="text"
-              placeholder="Your last name"
+              placeholder={t("register.lastNameInput")}
               className="input-registro"
             />
-            <label className="label-registro">Phone</label>
+            <label className="label-registro">{t("register.phoneLabel")}</label>
             <input
               type="text"
-              placeholder="Your phone number"
+              placeholder={t("register.phoneInput")}
               className="input-registro"
             />
-            <label className="label-registro">Birth Date</label>
+            <label className="label-registro">
+              {t("register.birthDateLabel")}
+            </label>
             <input
               type="date"
-              placeholder="Your birth date"
+              placeholder={t("register.birthDateInput")}
               className="input-registro"
             />
-            <label className="label-registro">Username</label>
+            <label className="label-registro">{t("register.userLabel")}</label>
             <input
               type="text"
-              placeholder="Your username"
+              placeholder={t("register.userInput")}
               className="input-registro"
               onChange={handleChange}
               name="userName"
             />
-            <label className="label-registro">Password</label>
+            <label className="label-registro">{t("register.password")}</label>
             <input
               type="text"
-              placeholder="Your password"
+              placeholder={t("register.passwordInput")}
               className="input-registro"
               onChange={handleChange}
               name="password"
             />
           </div>
           <button className="boton-registro" onClick={handleSubmit}>
-            Register
+            {t("register.btnRegister")}
           </button>
         </div>
       )}
       {added && (
         <div className="contenido-agregado">
-          <h3 className="titulo-agregado">Welcome</h3>
+          <h3 className="titulo-agregado">{t("register.welcome")}</h3>
           <p className="texto-agregado">
-            Your user account was successfully created
+            {t("register.welcomeMessage")}
           </p>
           <NavLink to="/" className="boton-agregado">
-            Continue
+            {t("register.btnContinue")}
           </NavLink>
         </div>
       )}
