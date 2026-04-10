b991a63c6cb6983dac447b9d1412d5af1dd0305d
Felipe Jaramillo <pipejarauribeboteroa@gmail.com>
2026-04-10 16:45:21 -0500

    mapa i18n listo

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,69 +1,71 @@
 import reactLogo from "../assets/react.svg";
 import { NavLink } from "react-router-dom";
+import {useTranslation} from 'react-i18next'
 import "../styles/Map.css";
 
 export const Map = () => {
+  const { t } = useTranslation("global");
   return (
     <div className="mapa">
       <a target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
-      <p className="tituloMapa">Navigation Map</p>
+      <p className="tituloMapa">{t("map.title")}</p>
       <ul className="lista">
         <li>
           <NavLink to="/" className="nav-link-mapa">
-            Home
+            {t("map.home")}
           </NavLink>
         </li>
         <li>
           <NavLink to="/store" className="nav-link-mapa">
-            Store
+            {t("map.store")}
           </NavLink>
         </li>
         <li>
           <NavLink to="/employes" className="nav-link-mapa">
-            Schedule
+            {t("map.schedule")}
           </NavLink>
           <ul>
             <li>
               <NavLink to="/new" className="nav-link-mapa">
-                New item
+                {t("map.newItem")}
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
           <NavLink to="/search" className="nav-link-mapa">
-            Search
+            {t("map.search")}
           </NavLink>
         </li>
         <li>
           <NavLink to="/tasks" className="nav-link-mapa">
-            Tasks
+            {t("map.tasks")}
           </NavLink>
           <ul>
             <li>
               <NavLink to="/newList" className="nav-link-mapa">
-                New list
+                {t("map.newList")}
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
           <NavLink to="/user" className="nav-link-mapa">
-            Log in
+            {t("map.login")}
           </NavLink>
           <ul>
             <li>
               <NavLink to="/register" className="nav-link-mapa">
-                Register
+                {t("map.register")}
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
           <NavLink to="/car" className="nav-link-mapa">
-            Shopping cart
+            {t("map.cart")}
           </NavLink>
         </li>
       </ul>
